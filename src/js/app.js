60fe91bb174823797d6b59ce6960bdf6693cc515
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-11-17 14:08:20 +0000

    adding react table

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import $ from 'jquery'
 import pkg from '../../package.json'
+import table from './table'
 
 const EXT_NAME = pkg.name
 const URL_PREFIX = Qva.Remote + (Qva.Remote.indexOf('?') >= 0 ? '&' : '?') + `public=only&name=Extensions/${EXT_NAME}/`
@@ -8,8 +9,6 @@ $(() => {
   Qva.LoadCSS(`${URL_PREFIX}asset/css/app.css`)
 
   Qv.AddExtension(EXT_NAME, function () {
-    console.log(this.Data.Rows)
-
-    this.Element.innerHTML = '<div class="some-element">hello!</div>'
+    table(this.Element)
   })
 })
